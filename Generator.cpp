d27c060b30db1c4344a33550f11b6ab8aba7c5d0
YuHeng <yuhchen21@gmail.com>
2015-05-08 23:22:17 -0400

    Started on Generator

---
+++ Generator.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <random>
+#include <chrono>
+#include <algorithm>
+#include <vector>
+
+#define DEBUG 1 // 1 to turn debug on
+
+typedef struct file_struct{
+  int id;
+  int size;
+} File;
+
+void usage(char *progName){
+  std::cerr << "Usage: " << progName << " <disk-size> <write-ratio [0-100]> <output-file>" << std::endl;
+}
+
+std::string start(int fileID){
+  std::stringstream ss;
+  ss << "START " << fileID << std::endl;
+  std::string instruction;
+  std::getline(ss, instruction);
+  return instruction;
+}
+
+std::string write(int fileID, int blockID){
+  std::stringstream ss;
+  ss << "WRITE " << fileID << " " << blockID << std::endl;
+  std::string instruction;
+  std::getline(ss, instruction);
+  return instruction;  
+}
+
+std::string read(int fileID, int blockID){
+  std::stringstream ss;
+  ss << "READ " << fileID << " " << blockID << std::endl;
+  std::string instruction;
+  std::getline(ss, instruction);
+  return instruction;  
+}
+
+std::string end(int fileID){
+  std::stringstream ss;
+  ss << "END " << fileID << std::endl;
+  std::string instruction;
+  std::getline(ss, instruction);
+  return instruction;  
+}
+
+int main(int argc, char **argv){
+  if(argc < 4){
+    usage(argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  
+  // extract command line arguments
+  std::stringstream ss;
+  int diskCapacity; ss << argv[1]; ss >> diskCapacity;
+  int writeRatio=10; ss << argv[2]; ss >> writeRatio;
+  char *ofileName = argv[3];
+  
+  // validate writeRatio
+  if( (writeRatio < 0) || (writeRatio > 100) ){
+    std::cerr << argv[0] << ": writeRatio out of range [0,100]: " << writeRatio << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  std::ofstream outfile;
+  outfile.open(ofileName);
+
+  // seed rng
+  std::chrono::high_resolution_clock::time_point time = std::chrono::high_resolution_clock::now();
+  std::minstd_rand rng(time.time_since_epoch().count());
+
+  /***** generate simluation sequence *****/
+  int diskSize = 0;
+  std::vector<File> aliveFiles;
+  int new_fid = 1;
+  int nFilesCreated = 0;
+
+  // create at least 1 file
+  File f;
+  f.id = new_fid++;
+  f.size = 1;
+  aliveFiles.push_back(f);
+  diskSize++;
+  nFilesCreated++;
+  outfile << start(f.id) << std::endl;
+  
+  do{
+    // start
+    if( ((rng()%10) < 2) && (nFilesCreated < diskCapacity/4) ){
+      // new file
+      f.id = new_fid++;
+      f.size = 1;
+      
+      // add to disk
+      aliveFiles.push_back(f);
+      diskSize++;
+      nFilesCreated++;
+      outfile << start(f.id) << std::endl;
+      continue;
+    }
+    
+    // choose a file
+    if(!aliveFiles.empty()){
+      std::random_shuffle(aliveFiles.begin(), aliveFiles.end());
+      f = aliveFiles.back();
+    }
+    
+    // write
+    if( (int)(rng() % 100) < writeRatio ){
+      // writeRatio% chance to write
+      int block = 1 + (rng() % f.size);
+      File newFile;
+      if(block == f.size){
+	// writing new block
+	diskSize++;
+	int new_fsize = f.size + 1;
+	int new_fid = f.id;
+	newFile.id = new_fid;
+	newFile.size = new_fsize;
+	aliveFiles.pop_back();
+	aliveFiles.push_back(newFile);
+      }
+      outfile << write(newFile.id, block) << std::endl;
+      continue;
+    }
+    
+    // end/read
+    if(rng() % 100 == 0){
+      // end
+      aliveFiles.pop_back();
+      outfile << end(f.id) << std::endl;
+    }
+    else{
+      // (100-writeRatio)% chance to read
+      if(f.size > 1){
+	int block = 1 + (rng() % (f.size-1)); 
+	outfile << read(f.id, block) << std::endl;
+      }
+    }
+    
+  } while(diskSize < diskCapacity);
+  
+  // check that all files have ended, if not then clean up
+  while(!aliveFiles.empty()){
+    f = aliveFiles.back();
+    aliveFiles.pop_back();
+    outfile << end(f.id) << std::endl;
+  }
+  
+  return 0;
+}
--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 EXE = LFS-Sim
+GEN = GenLFS-Sim
 
 SRC = \
 	Driver.cpp \
-	LFS_Sim.cpp \
+	LFS_Sim.cpp 
+
+GEN_SRC = \
+	Generator.cpp
 
 OBJ_FILES := $(SRC:.cpp=.o)
+GEN_OBJ_FILES := $(GEN_SRC:.cpp=.o)
 
 GCC = g++
 FLAGS = -g -Wall
@@ -15,9 +20,13 @@ all:	$(EXE)
 $(EXE):	$(OBJ_FILES)
 	$(GCC) $(FLAGS) $(COMPILER) $(OBJ_FILES) -o $@
 
+gen:	$(GEN_OBJ_FILES)
+	$(GCC) $(FLAGS) $(COMPILER) $(GEN_OBJ_FILES) -o $(GEN)
+
+
 # common build recipe for .o
 %.o:	%.cpp
 	$(GCC) $(FLAGS) $(COMPILER) -c $^
 
 clean:
-	rm -f *.o $(EXE) $(GEN)
+	rm -f *~ *# *.o *.exe $(EXE) $(GEN)
